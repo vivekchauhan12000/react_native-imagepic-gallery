6265aec2dbe929e1e22632af302fdfcdb897bb80
vivek <vivekchauhaniit@gmail.com>
2020-04-10 20:36:14 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { Text, Image, View ,TouchableOpacity} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+
+export default class ImagePickerExample extends React.Component {
+  state = {
+    image: null,
+  };
+
+  render() {
+    let { image } = this.state;
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center',backgroundColor:'#00BCD4', }}>
+       
+        {image && <Image source={{ uri: image }} style={{ width: '100%', height: '90%' }} />}
+
+        <TouchableOpacity style={{backgroundColor:'green',margin:1,padding:10, borderRadius:10,}}
+        onPress={this._pickImage}>
+          <Text style={{color:'#fff'}}>Select Image from gallery</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  componentDidMount() {
+    this.getPermissionAsync();
+  }
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  };
+
+  _pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [16, 16],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        this.setState({ image: result.uri });
+      }
+
+      console.log(result);
+    } catch (E) {
+      console.log(E);
+    }
+  };
+}
